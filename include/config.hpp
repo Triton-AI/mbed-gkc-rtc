cbf733bc3e44de111288a5841344a5b512381179
WinstonHChou <winston.h.chou@gmail.com>
2026-02-13 13:08:16 -0800

    Fix casing for tower light red definition in config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -34,7 +34,7 @@
 #define SEND_SENSOR_INTERVAL_MS        20      // sensor packet send interval
 
 // Tower light indicators
-#define tower_light_red                pd_15 //(wiring) relay switch "lights"
+#define TOWER_LIGHT_RED                PD_15 //(wiring) relay switch "lights"
 #define TOWER_LIGHT_YELLOW             PD_11 //(wiring) relay switch "lights"
 #define TOWER_LIGHT_GREEN              PE_12 //(wiring) relay switch "lights"
 

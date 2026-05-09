8147a59e2004b1c716b31ebe55b51617e824a996
nicolas toon <110261244+nicolastoon@users.noreply.github.com>
2026-05-08 22:56:02 -0700

    Aryan/fix throttle brake (#10)
    * noic
    
    * brake was overactuating again
    
    * fix spacing for easier mergeability
    
    ---------
    
    Co-authored-by: aryan-palaskar <aryan.palaskar3@gmail.com>

--- include/config.hpp
+++ include/config.hpp
@@ -91,7 +91,7 @@
 // Brake pressure limits (PSI)
 #define MIN_BRAKE_VAL               600
 #define MAX_BRAKE_VAL               3000
-#define EMERGENCY_BRAKE_PRESSURE    0.4f  // override brake pressure in emergency
+#define EMERGENCY_BRAKE_PRESSURE    0.35f  // override brake pressure in emergency
 /*
 <= 0.1: 100PSI
 0.2: 250PSI
